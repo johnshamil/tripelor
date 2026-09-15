a038ba6fbc73dcf9e32a8443f8e9ed616f5ef57b
johnshamil <johnshamil87@gmail.com>
2026-09-15 11:51:38 +0500

    Add admin-managed Know Before You Book details

--- components/managed-property-view.tsx
+++ components/managed-property-view.tsx
@@ -1,3 +1,4 @@
+import PropertyKnowGuide from "@/components/property-know-guide";
 import PropertyArrivalGuide from "@/components/property-arrival-guide";
 import PropertyExperiences from "@/components/property-experiences";
 import LuxuryPropertyPage from "@/components/luxury-property-page";
@@ -69,5 +70,5 @@ export default function ManagedPropertyView({ property, preview = false }: { pro
       { title: "Cancellation", text: property.cancellation },
       { title: "Payment", text: property.payment },
     ]}
-  /><PropertyArrivalGuide property={property} preview={preview}/><PropertyExperiences experiences={property.experiences || []} propertyName={property.name} slug={property.slug} preview={preview}/></>;
+  /><PropertyKnowGuide property={property} preview={preview}/><PropertyArrivalGuide property={property} preview={preview}/><PropertyExperiences experiences={property.experiences || []} propertyName={property.name} slug={property.slug} preview={preview}/></>;
 }
